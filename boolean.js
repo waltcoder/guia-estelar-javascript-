6fa504c1642949aa2d16cd9e23a37e37ed211a2f
Walt Johnsley Cerilus <thommyjonesprograming@gmail.com>
2022-04-14 13:50:21 -0300

     add boolean.js

---
+++ boolean.js
@@ -0,0 +1,22 @@
+/**
+ * Boolean - George Boole
+ *  Booleano
+ * 
+ * Retorna somente dois valores 
+ *    true // verdadeiro 
+ *    false // falso
+ *  */
+
+
+console.log(false);
+console.log(true);
+
+const coffee = true;
+console.log(coffee);
+console.log(!coffee);
+
+if(!coffee) {
+  console.log('não tomou café');
+} else{ 
+   console.log('tomou café');
+}
