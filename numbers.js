d0445d25e1912bf195bfaeabf6840f96140f5eb4
Walt Johnsley Cerilus <thommyjonesprograming@gmail.com>
2022-04-14 13:36:58 -0300

     add numbers.js

---
+++ numbers.js
@@ -0,0 +1,14 @@
+/**
+ * Number 
+ * 
+ * 33 // Inteiros
+ * 23.5 // Reais - float
+ * NaN // Not a Number
+ * Infinity // infinito
+ */
+
+
+console.log(33);
+console.log(23.5);
+console.log(35345 / 10);
+console.log(23423 / 'waltcoder');
