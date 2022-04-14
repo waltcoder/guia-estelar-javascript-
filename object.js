f56c055390be2b2c9e4a5c165dfe686a069aecff
Walt Johnsley Cerilus <thommyjonesprograming@gmail.com>
2022-04-14 14:07:31 -0300

     add object.js

---
+++ object.js
@@ -0,0 +1,18 @@
+/**
+ * Object 
+    * Objeto 
+    *  Propriedades / Atributos
+    *   Funcionalidade / Métodos
+    * 
+    * { propriedade : valor }
+    * É um tipo de dado estrutural, estou estruturando meus dados
+ */
+
+console.log({
+   name: "Walt Johnsley C.",
+   username: "@waltcoder",
+   idade: "17 anos",
+   hobbies: function () {
+      console.log('O walt não passa de um vagabundo metido a besta, kkkkkkk')
+   } 
+})
