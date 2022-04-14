752548c741f7da72f340d3efe7ca8bcb5dd3f9e5
Walt Johnsley Cerilus <thommyjonesprograming@gmail.com>
2022-04-14 13:57:33 -0300

     add undefined.js

---
+++ undefined.js
@@ -0,0 +1,12 @@
+/**
+ * Undefined 
+    * indefinido 
+    * Algo que não vai existir
+* null 
+    * null 
+    * objeto que não possui nada dentro 
+    * diferente de indefinido
+ */
+
+
+console.log( null === undefined );
