5ceb2de3cf64e49421cea7c6f79788de96243c8b
Walt Johnsley Cerilus <thommyjonesprograming@gmail.com>
2022-04-14 13:36:38 -0300

     add strings.js

---
+++ strings.js
@@ -0,0 +1,11 @@
+/*
+* Strings
+"" // aspas duplas 
+'' // simples
+`` // template literals ou template strings | permitem multi-linhas
+*/
+
+
+console.log("Hello World");
+console.log('Hello World');
+console.log(`Hello World ${6 + 5}`);
