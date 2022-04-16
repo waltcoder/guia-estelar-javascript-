52d87a332086bb6e7d16492f54cfa86a55f38f43
Walt Johnsley Cerilus <thommyjonesprograming@gmail.com>
2022-04-16 18:21:37 -0300

     add praticando-e-avançando

---
+++ praticando-e-avançando/array.js
@@ -0,0 +1,12 @@
+const animals = [
+  'Lion',
+  'Monkey',
+  'Cat',
+  'Dog'
+];
+
+// acessar valores dentro do array
+console.log(animals);
+console.log(animals[1])
+
+console.log(animals.length);
---
+++ praticando-e-avançando/object.js
@@ -0,0 +1,13 @@
+// Object
+const person = {
+  name : 'Walt',
+  age : 17,
+  weight : 50,
+  isAdmin : true,
+}
+
+// Imprimindo propriedades do objeto person
+console.log(person);
+console.log(person.name);
+
+console.log(`${person.name} tem ${person.age} anos`);
