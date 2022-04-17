a3baa3fd63eb95d24cfaa1dd6af075a6643351f8
Walt Johnsley Cerilus <thommyjonesprograming@gmail.com>
2022-04-17 17:25:12 -0300

     add javascript tips

---
+++ javascript-tips/convert-number-into-binary-or-hexadecimal.js
@@ -0,0 +1,4 @@
+// Use to 'toString' method to convert a number into binary or hexadecimal ---- or any format for that matter.
+const number = 36;
+console.log(number.toString(2));
+console.log(number.toString(16));
